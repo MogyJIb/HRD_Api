07e4428878889a9e9a1981d74b08c1756d165b17
Evgeniy Shevtsov <eshevtsov@intervale.ru>
2018-09-04 14:20:26 +0300

    Refactor departments controller with session

--- HRD_Api/Controllers/DepartmentsController.cs
+++ HRD_Api/Controllers/DepartmentsController.cs
@@ -26,46 +26,50 @@ namespace HRD_Api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetDepartments(string session, bool deleted = false)
         {
-            if (SessionLogic.Instance.Valid(session))
-                return Json(_context.Departments.Where(department => department.Deleted == deleted));
-            else
+            if (!SessionLogic.Instance.Valid(session))
             {
                 Response.StatusCode = 403;
                 return Json(ErrorType.AuthanticationFaild);
             }
+            
+            return Json(_context.Departments.Where(department => department.Deleted == deleted));
         }
 
         // GET: api/departments/5
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetDepartment([FromRoute] int id)
+        public async Task<IActionResult> GetDepartment(string session, [FromRoute] int id)
         {
-            if (!ModelState.IsValid)
+            if (!SessionLogic.Instance.Valid(session))
             {
-                return BadRequest(ModelState);
+                Response.StatusCode = 403;
+                return Json(ErrorType.AuthanticationFaild);
             }
 
             var department = await _context.Departments.SingleOrDefaultAsync(m => m.DepartmentId == id);
 
             if (department == null)
             {
-                return NotFound();
+                Response.StatusCode = 405;
+                return Json(ErrorType.NotFoundObject);
             }
 
-            return Ok(department);
+            return Json(department);
         }
 
         // PUT: api/departments/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutDepartment([FromRoute] int id, [FromBody] Department department)
+        public async Task<IActionResult> PutDepartment(string session, [FromRoute] int id, [FromBody] Department department)
         {
-            if (!ModelState.IsValid)
+            if (!SessionLogic.Instance.Valid(session))
             {
-                return BadRequest(ModelState);
+                Response.StatusCode = 403;
+                return Json(ErrorType.AuthanticationFaild);
             }
-
+            
             if (id != department.DepartmentId)
             {
-                return BadRequest();
+                Response.StatusCode = 405;
+                return Json(ErrorType.NotFoundObject);
             }
 
             _context.Entry(department).State = EntityState.Modified;
@@ -73,56 +77,52 @@ namespace HRD_Api.Controllers
             try
             {
                 await _context.SaveChangesAsync();
+                return NoContent();
             }
-            catch (DbUpdateConcurrencyException)
+            catch (Exception)
             {
-                if (!DepartmentExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                Response.StatusCode = 500;
+                return Json(ErrorType.InternalError);
             }
-
-            return NoContent();
         }
 
         // POST: api/departments
         [HttpPost]
-        public async Task<IActionResult> PostDepartment([FromBody] Department department)
+        public async Task<IActionResult> PostDepartment(string session, [FromBody] Department department)
         {
-            if (!ModelState.IsValid)
+            if (!SessionLogic.Instance.Valid(session))
             {
-                return BadRequest(ModelState);
+                Response.StatusCode = 403;
+                return Json(ErrorType.AuthanticationFaild);
             }
 
             _context.Departments.Add(department);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetDepartment", new { id = department.DepartmentId }, department);
+            return await GetDepartment(session, department.DepartmentId);
         }
 
         // DELETE: api/departments/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteDepartment([FromRoute] int id)
+        public async Task<IActionResult> DeleteDepartment(string session, [FromRoute] int id)
         {
-            if (!ModelState.IsValid)
+            if (!SessionLogic.Instance.Valid(session))
             {
-                return BadRequest(ModelState);
+                Response.StatusCode = 403;
+                return Json(ErrorType.AuthanticationFaild);
             }
 
             var department = await _context.Departments.SingleOrDefaultAsync(m => m.DepartmentId == id);
             if (department == null)
             {
-                return NotFound();
+                Response.StatusCode = 405;
+                return Json(ErrorType.NotFoundObject);
             }
 
             _context.Departments.Remove(department);
             await _context.SaveChangesAsync();
 
-            return Ok(department);
+            return Json(department);
         }
 
         private bool DepartmentExists(int id)
